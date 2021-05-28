282ae73f2c7a4c8a08ca2597e4a13a56a10e9e27
Alexander <51728129+Alex-T-RU-DE@users.noreply.github.com>
2021-05-28 18:02:44 +0200

    Update Gripper_service_server.ino

--- Gripper_service_server.ino
+++ Gripper_service_server.ino
@@ -21,7 +21,7 @@ void setup()
                nh.spinOnce();
            nh.loginfo("Gripper ready.");
            SERVO.WritePos(1, 925, 0x01FF);
-           SERVO.WritePos(12,90, 0x01FF);
+           SERVO.WritePos(12, 90, 0x01FF);
 }
 
 void loop()
@@ -33,39 +33,39 @@ void loop()
 bool add(youbot_gripper::grip_service::Request  &req,
          youbot_gripper::grip_service::Response &res)
 {
- res.result=false;
- nh.loginfo("Got new request."); 
- if(data_check(req.pos) && safety_check())
-{
-  switch(static_cast<int>(req.pos/1000))
+   res.result = false;
+   nh.loginfo("Got new request."); 
+   if(data_check(req.pos) && safety_check())
+   {
+    switch(static_cast<int>(req.pos / 1000))
              {    case 0:
-                         SERVO.WritePos(1, (930-map(req.pos,0,100,0,380)), 0x01FF);
-                         SERVO.WritePos(12,(90 +map(req.pos,0,100,0,380)), 0x01FF);
-                         int last_servo_load=SERVO.Get_Load(1);
-                         int pos_error=30;  //assuming the data might have uncertainty
-                         int load_error=50; //assuming the data might have uncertainty
-                         int grasping_load=1000; 
+                         SERVO.WritePos(1, (930 - map(req.pos, 0, 100, 0, 340)), 0x01FF);
+                         SERVO.WritePos(12,(90  + map(req.pos, 0, 100, 0, 340)), 0x01FF);
+                         int lastServoLoad = SERVO.Get_Load(1);
+                         const int posError  = 30;  //assuming the data might have uncertainty
+                         const int loadError = 50; //assuming the data might have uncertainty
+                         const int graspingLoad = 1000; 
                          //checking whether object was grasped or not
-                         while(abs(SERVO.Get_Pos(1)-(930-map(req.pos,0,100,0,380)))>pos_error)
+                         while(abs(SERVO.Get_Pos(1) - (930 - map(req.pos, 0, 100, 0, 340))) > posError)
                              {
                               delay(500);
-                              if((abs(last_servo_load-SERVO.Get_Load(1))<=load_error) && (SERVO.Get_Load(1)>grasping_load))
+                              if((abs(lastServoLoad - SERVO.Get_Load(1))<=loadError) && (SERVO.Get_Load(1) > graspingLoad))
                                   break;
-                               last_servo_load=SERVO.Get_Load(1);                            
+                               lastServoLoad = SERVO.Get_Load(1);                            
                              }
                           delay(200);
-                          if(SERVO.Get_Load(1)>grasping_load)
+                          if(SERVO.Get_Load(1)>graspingLoad)
                             {
                              nh.loginfo("Object grasped.");                        
                              res.result = true;
                             }                     
                          break;
                   case 1:
-                         SERVO.WritePos(1,(930-(req.pos-1000)), 0x01FF);
+                         SERVO.WritePos(1, (930 - (req.pos - 1000)), 0x01FF);
                          res.result = true;                    
                          break;
                   case 2:
-                         SERVO.WritePos(12,(90+(req.pos-2000)), 0x01FF);
+                         SERVO.WritePos(12, (90 + (req.pos - 2000)), 0x01FF);
                          res.result = true;
                          break;
                   default:
@@ -73,9 +73,12 @@ bool add(youbot_gripper::grip_service::Request  &req,
                          res.result = false;
           
              }
-  }
-  else 
-          res.result = false;     
+  } else 
+  {
+        nh.logerror("Invalid command: the number should be in range(0:100; 1000:1380; 2000;2380.");
+        res.result = false;
+        return false;
+  } 
   nh.loginfo("Sending back response.");
   return true;
 }
@@ -85,17 +88,17 @@ bool safety_check()
       checking actual voltage, position and temperature
       More info about limits: http://image.dfrobot.com/image/data/SER0026/cds55xx-robot-servo-datasheet.pdf
     */
-    if(SERVO.Get_Voltage(1)<=6.6 || SERVO.Get_Voltage(1)>=16)
+    if(SERVO.Get_Voltage(1) <= 6.6 || SERVO.Get_Voltage(1) >= 16)
       {
         nh.logerror("Aborted. Unsafe voltage");
         return false;
       }
-    if(SERVO.Get_Temp(1)<=-20 || SERVO.Get_Temp(1)>=80)
+    if(SERVO.Get_Temp(1) <= -20 || SERVO.Get_Temp(1) >= 80)
       {
         nh.logerror("Aborted. Unsafe temperature");
         return false;
       }
-    if(SERVO.Get_Pos(1)<525 || SERVO.Get_Pos(1)>947)
+    if(SERVO.Get_Pos(1) < 525 || SERVO.Get_Pos(1) > 947)
       {
         nh.logerror("Aborted. Unsafe position of the fitst servo");
         return false;
@@ -106,13 +109,6 @@ bool safety_check()
 
 bool data_check(int number) //checking the command
   {
-    if((number>=0) || (number<=100)
-    || (number>=1000 && number<=1380)
-    || (number>=2000 && number<=2380))
-        return true;
-    else
-        {
-        nh.logerror("Invalid command: the number should be in range(0:100; 1000:1380; 2000;2380.");
-        return false;
-        }
+    return ((number >= 0  && number <= 100) || (number >= 1000 && number <= 1340) || (number >= 2000 && number <= 2340))
+
   }
